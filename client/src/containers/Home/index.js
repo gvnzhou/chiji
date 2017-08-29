c27b729a241cdb60868cf464c3954c03bf46d3f4
javion25 <228806684@qq.com>
2017-08-29 17:49:40 +0800

    fix code styles

--- client/src/containers/Home/index.js
+++ client/src/containers/Home/index.js
@@ -1,30 +1,25 @@
 /**
  * Created by h9zhou on 2017/5/5.
  */
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class Home extends Component {
-	// constructor (props) {
-	// 	super(props)
-	// }
-
-	componentDidMount() {
-		document.title = '吃鸡吧'
+  componentDidMount() {
+    document.title = '吃鸡吧';
   }
 
-	render() {
-		return (
-			<div>
-				<header>
-					logo
-				</header>
+  render() {
+    return (
+      <div>
+        <header>
+          logo
+        </header>
         <nav>
-					导航12323sawdada
-				</nav>
-			</div>
-		)
-	}
+          导航12323sawdada
+        </nav>
+      </div>
+    )
+  }
 }
 
-
-export default Home
+export default Home;
